d672b1ef5c51d3b173faa0c158f33bef4d0266ae
drew-codes <hello@drewthompson.dev>
2024-03-03 13:55:07 -0500

    fix: tweak game area

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ async fn main() {
     loop {
         let padding = 20.0;
         let screen_w = screen_width();
-        let square_size = (screen_w / 2.0) - (padding * 2.0);
-        let x_position = (screen_w - square_size) / 2.0;
-        let y_position = x_position; // Assuming you want to center it vertically as well
+        let square_size = screen_w * 0.35;
+        let x_position = 0. + padding;
+        let y_position = 0. + padding;
 
         // Clear the background
-        clear_background(BLUE);
+        clear_background(WHITE);
 
         // Draw the square
         draw_rectangle(x_position, y_position, square_size, square_size, DARKGRAY);
