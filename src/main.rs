9d1ff71899b5fdb15a21c1d41a2a05ccac751588
drew-codes <hello@drewthompson.dev>
2024-03-02 19:39:00 -0500

    WIP: game area

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@ use macroquad::prelude::*;
 #[macroquad::main("BasicShapes")]
 async fn main() {
     loop {
-        clear_background(RED);
+        let padding = 20.0;
+        let screen_w = screen_width();
+        let square_size = (screen_w / 2.0) - (padding * 2.0);
+        let x_position = (screen_w - square_size) / 2.0;
+        let y_position = x_position; // Assuming you want to center it vertically as well
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        // Clear the background
+        clear_background(BLUE);
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        // Draw the square
+        draw_rectangle(x_position, y_position, square_size, square_size, DARKGRAY);
 
+        // Move to the next frame
         next_frame().await
     }
 }
